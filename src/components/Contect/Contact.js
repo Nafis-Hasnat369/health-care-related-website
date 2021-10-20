4699c6e4357b213ea19f2d1924138737a26daa96
Nafis-Hasnat369 <nafis7823@gmail.com>
2021-10-20 23:54:33 +0600

    everything added

--- src/components/Contect/Contact.js
+++ src/components/Contect/Contact.js
@@ -3,21 +3,23 @@ import { Col, Row } from 'react-bootstrap';
 
 const Contact = () => {
     return (
-        <Row>
-            <h2>Contuct us</h2>
-            <Col lg={4} md={6} sm={12}>
-                Name: Dr. Payel
-                Mobile: +880123456789
-            </Col>
-            <Col lg={4} md={6} sm={12}>
-                Name: Dr. Raj
-                Mobile: +880123456789
-            </Col>
-            <Col lg={4} md={6} sm={12}>
-                Name: Dr. Rahman
-                Mobile: +880123456789
-            </Col>
-        </Row>
+        <div style={{ height: "90vh" }} className="mt-5">
+            <Row>
+                <h2>Contuct us</h2>
+                <Col lg={4} md={6} sm={12}>
+                    Name: Dr. Payel
+                    Mobile: +880123456789
+                </Col>
+                <Col lg={4} md={6} sm={12}>
+                    Name: Dr. Raj
+                    Mobile: +880123456789
+                </Col>
+                <Col lg={4} md={6} sm={12}>
+                    Name: Dr. Rahman
+                    Mobile: +880123456789
+                </Col>
+            </Row>
+        </div>
     );
 };
 
